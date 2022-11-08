7eb2711484588a2658492f03020d5f007e4f6e6e
Nis-chal <nischalchan@gmail.com>
2022-11-08 06:34:52 +0545

    login design layout was done

--- frontend/src/pages/loginPage/wrapper/loginpage.js
+++ frontend/src/pages/loginPage/wrapper/loginpage.js
@@ -1,7 +1,111 @@
 import styled from "styled-components";
 
 const Wrapper = styled.div`
+  .login-container {
+    display: flex;
+    height: 100vh;
+    background-color: white;
+    overflow: hidden;
+    justify-content: space-between;
 
+    & > .back-arrow {
+      position: absolute;
+      top: 5%;
+      left: 5%;
+      font-size: 2.5rem;
+    }
+  }
 
-`
-export default Wrapper
+  .login-heading {
+    font-size: 3rem;
+  }
+
+  .form-container {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    text-transform: uppercase;
+
+    padding: 10%;
+    gap: 20px;
+
+    & > .btn-submit {
+      margin-top: 40px;
+      width: 100%;
+      padding: 2%;
+
+      border: none;
+      color: whitesmoke;
+      font-weight: bolder;
+      background-color: var(--primary-color);
+      cursor: pointer;
+    }
+
+    & > .toggle-account {
+      text-transform: capitalize;
+      color: var(--black-500);
+      align-self: flex-end;
+      cursor: pointer;
+    }
+  }
+
+  .image-container {
+    width: 100%;
+    background-color: var(--primary-color);
+    position: relative;
+
+    & > .login-info {
+      position: absolute;
+      top: 20%;
+      left: 40%;
+      z-index: 2;
+      font-size: min(70px, calc(20px + 4vw));
+      color: white;
+    }
+
+    & > img {
+      margin-top: 70px;
+      width: 70%;
+      object-fit: contain;
+      transform: translateX(-40%);
+      animation: bounce infinite 15.2s forwards;
+    }
+  }
+
+  @keyframes hoverd {
+    0% {
+      transform: translateY(-100px);
+    }
+
+    50% {
+      transform: translateY(-50px);
+    }
+
+    100% {
+      transform: translateY(-100px);
+    }
+  }
+
+  @keyframes bounce {
+    0%,
+    20%,
+    50%,
+    80%,
+    100% {
+      transform: translateY(0) translateX(-40%);
+    }
+    40% {
+      transform: translateY(-30px) translateX(-40%);
+    }
+    60% {
+      transform: translateY(-15px) translateX(-40%);
+    }
+  }
+
+  @media screen and (max-width: 700px) {
+    .image-container {
+      display: none;
+    }
+  }
+`;
+export default Wrapper;
