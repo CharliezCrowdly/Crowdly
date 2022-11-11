b1b557174851e855f9840dd2a2521ec22ef54dac
Manish <silwalm7553@gmail.com>
2022-11-11 22:13:45 +0545

    post upload section basic layout completed

--- frontend/src/pages/FeedPage/FeedPage.js
+++ frontend/src/pages/FeedPage/FeedPage.js
@@ -1,5 +1,6 @@
 import React, { useState, useRef } from "react";
 import Wrapper from "./wrapper/FeedPage";
+import Searchbar from "./component/Searchbar";
 
 import lstrecommendation from "../../utils/lstrecommendation";
 
@@ -9,7 +10,10 @@ const FeedPage = () => {
   return (
     <Wrapper>
       <div className="feedPage">
-        <div className="middle-container"></div>
+        <div className="middle-container">
+          
+          <Searchbar  />
+        </div>
         <div className="right-container">
           {lstrecommendation.map((item) => (
             <Recommendation item={item} key={item.id} />
---
+++ frontend/src/pages/FeedPage/component/Searchbar.js
@@ -0,0 +1,197 @@
+import React, { useState } from "react";
+import { RiImageAddFill } from "react-icons/ri";
+import { HiLocationMarker } from "react-icons/hi";
+import { FcVideoFile } from "react-icons/fc";
+import { AiFillFile } from "react-icons/ai";
+import Wrapper from "../wrapper/Searchbar";
+
+const uploadState = {
+  location: false,
+  image: false,
+  video: false,
+  audio: false,
+  other: false,
+  userLocation: "",
+  postfile: "",
+  description: "",
+  filetype: "",
+};
+
+const Searchbar = React.memo(({  toggleload }) => {
+  const [upload, setUpload] = useState(uploadState);
+  const [preview, setPreview] = useState("");
+
+  const toggleLocation = (e) => {
+    setUpload({ ...upload, location: !upload.location });
+  };
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setUpload({ ...upload, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit =(e) => {
+    e.preventDefault();
+    const { postfile, userLocation, description, filetype } = upload;
+
+    const userpost = { postfile, userLocation, description, filetype };
+
+   
+  
+        
+  };
+
+  const onFileSelection = (e) => {
+    var file = e.target.files[0];
+    console.log(e.target.files[0].type.substring(0, file.type.indexOf("/")));
+    console.log(e.target.files[0].type);
+
+    if (file.type.match("video.*")) {
+      setUpload({
+        ...upload,
+        [e.target.name]: file,
+        filetype: e.target.files[0].type,
+        image: false,
+        video: true,
+        audio: false,
+        other: false,
+      });
+    }
+
+    if (file.type.match("image.*")) {
+      setUpload({
+        ...upload,
+        [e.target.name]: file,
+        filetype: e.target.files[0].type,
+
+        image: true,
+        video: false,
+        audio: false,
+        other: false,
+      });
+    }
+
+    if (file.type.match("audio.*")) {
+      setUpload({
+        ...upload,
+        [e.target.name]: file,
+        filetype: e.target.files[0].type,
+
+        image: false,
+        video: false,
+        audio: true,
+        other: false,
+      });
+    }
+
+    if (
+      !file.type.match("audio.*") &&
+      !file.type.match("video.*") &&
+      !file.type.match("image.*")
+    ) {
+      setUpload({
+        ...upload,
+        [e.target.name]: file,
+        filetype: e.target.files[0].type,
+
+        image: false,
+        video: false,
+        audio: false,
+        other: true,
+      });
+    }
+
+    if (e.target.files && e.target.files[0]) {
+      let reader = new FileReader();
+
+      reader.onload = function () {
+        setPreview(reader.result);
+      };
+      reader.readAsDataURL(e.target.files[0]);
+    }
+  };
+  return (
+    <Wrapper>
+      <div className="feedsearcharea">
+        <div className="feedsearch glassmorphism">
+          <img
+            className="profile-pic-sm"
+            src="https://i.pinimg.com/736x/0d/0b/21/0d0b211c1ecc0562b9f4c2a4305c9436.jpg"
+            alt=""
+          />
+
+          <input
+            type="textarea"
+            placeholder="enter description"
+            className="searchbar"
+            name="description"
+            value={upload.description}
+            onChange={handleChange}
+          />
+          <div className="fileupload">
+            <input
+              type="file"
+              name="postfile"
+              className="uploadfile"
+              id=""
+              onChange={onFileSelection}
+            />
+            <RiImageAddFill className="icon" />
+          </div>
+          <HiLocationMarker className="icon" onClick={toggleLocation} />
+          <button className="btn-send"  onClick={onSubmit}>
+            send
+          </button>
+        </div>
+
+        <input
+          type="text"
+          name="userLocation"
+          className={
+            upload.location ? "location-input glassmorphism" : "d-none"
+          }
+          placeholder="Enter location"
+          value={upload.userLocation}
+          onChange={handleChange}
+        />
+
+        <div className="imagefield ">
+          {upload.audio ? (
+            <div className="audio-select glassmorphism">
+              <AiFillFile className="icon" />{" "}
+              <div className="updated"> Audio Selected</div>
+            </div>
+          ) : (
+            <div></div>
+          )}
+          {upload.image ? (
+            <div className="img-select glassmorphism">
+              <img src={preview} className="update-img" alt="" />
+              <span>Image selected</span>
+            </div>
+          ) : (
+            <div></div>
+          )}
+          {upload.video ? (
+            <div className="video-select glassmorphism">
+              <FcVideoFile className="icon" />{" "}
+              <div className="updated"> Video Selected</div>
+            </div>
+          ) : (
+            <div></div>
+          )}
+          {upload.other ? (
+            <div className="file-select glassmorphism">
+              <AiFillFile className="icon" />{" "}
+              <div className="updated"> file Selected</div>
+            </div>
+          ) : (
+            <div></div>
+          )}
+        </div>
+      </div>
+    </Wrapper>
+  );
+});
+
+export default Searchbar;
