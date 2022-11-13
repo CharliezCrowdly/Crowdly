0638cc15726cba0bc8c3621bd60d8093505fbcb3
Nis-chal <nischalchan@gmail.com>
2022-11-13 17:42:05 +0545

    post design was done

--- frontend/src/pages/FeedPage/FeedPage.js
+++ frontend/src/pages/FeedPage/FeedPage.js
@@ -1,18 +1,19 @@
-import React, { useState, useRef } from "react";
+import React from "react";
 import Wrapper from "./wrapper/FeedPage";
 import Searchbar from "./component/Searchbar";
 
 import lstrecommendation from "../../utils/lstrecommendation";
 
 import { Recommendation } from "../../component";
+import { PostContainer } from "./component";
 
 const FeedPage = () => {
   return (
     <Wrapper>
       <div className="feedPage">
         <div className="middle-container">
-          
-          <Searchbar  />
+          <Searchbar />
+          <PostContainer />
         </div>
         <div className="right-container">
           {lstrecommendation.map((item) => (
