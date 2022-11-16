8af18b87282a4637f48f6dbb31b72db0615ff77d
Chirag Simkhada <chiragsimkhada@gmail.com>
2022-11-16 19:37:55 +0545

    Completed Unit testing for comment

--- backend/test/post.test.js
+++ backend/test/post.test.js
@@ -44,3 +44,48 @@ it("Post / unlike ===> 200 on success ", () => {
     .expect("Content-Type", /json/)
     .expect(200);
 });
+
+it("Post / Comment ===> 201 on success ", () => {
+  return request(app)
+    .post("/api/v1/comment/post")
+    .send({
+      postId: "6373b72313a2ae8caa653af8",
+      content: "Nice Picture",
+    })
+    .set("Authorization", `Bearer ${token}`)
+    .expect(200);
+});
+
+it("Post / Delete ===> 201 on success ", () => {
+  return request(app)
+    .delete("/api/v1/comment/delete/6374e0e45a982bda19ada7b1")
+    .set("Authorization", `Bearer ${token}`)
+    .expect(200);
+});
+
+it("Post / Get ===> 201 on success ", () => {
+  return request(app)
+    .get("/api/v1/comment/get/6373ad2413a2ae8caa653acf")
+    .set("Authorization", `Bearer ${token}`)
+    .expect(200);
+});
+
+it("Post / Update ===> 401 on Unauthorized comment manipulation ", () => {
+  return request(app)
+    .patch("/api/v1/comment/update/6373ad2413a2ae8caa653acf")
+    .send({
+      content: "Nice Picture",
+    })
+    .set("Authorization", `Bearer ${token}`)
+    .expect(401);
+});
+
+it("Post / Update ===> 200 on successful comment edit ", () => {
+  return request(app)
+    .patch("/api/v1/comment/update/6374eadf6a6555b7f09786b2")
+    .send({
+      content: "Nice Picture",
+    })
+    .set("Authorization", `Bearer ${token}`)
+    .expect(200);
+});
