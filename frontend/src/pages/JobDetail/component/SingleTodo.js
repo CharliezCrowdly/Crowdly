e3401bd5cbb5525acbd0b808d527b7852c222a16
Nis-chal <Nischalchan@gmail.com>
2022-11-25 21:57:23 +0545

    todo completed

--- frontend/src/pages/JobDetail/component/SingleTodo.js
+++ frontend/src/pages/JobDetail/component/SingleTodo.js
@@ -1,13 +1,61 @@
-import React,{useState} from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { MdDelete } from "react-icons/md";
-
+import { useAppContext } from "../../../context/appContext";
+import axios from "axios";
 const SingleTodo = ({ item, ondelete }) => {
-  const [Done, setDone] = useState(false);
+  const { tasktype, text, userid, _id } = item;
+  const [Done, setDone] = useState(tasktype === "done" ? true : false);
+  const { token } = useAppContext();
+  const load = useRef(true);
+
+  useEffect(() => {
+    if (load.current === true) {
+      if (tasktype === "done") {
+        setDone(true);
+      } else {
+        setDone(false);
+      }
+    }
+  }, [tasktype]);
+
+  const toggleDone = async () => {
+    if (!Done) {
+      await axios
+        .delete(
+          `/api/v1/todo/update/${_id}`,
+
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        )
+        .then((res) => {
+          setDone(true);
+        });
+    } else {
+      await axios
+        .delete(
+          `/api/v1/todo/unupdate/${_id}`,
+
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        )
+        .then((res) => {
+          setDone(false);
+        });
+    }
+  };
 
   return (
     <div className="lists">
-      <span onClick={()=> setDone(Done => !Done)}  className={Done ? "done":""}>{item}</span>
-      <MdDelete className="delete" onClick={() => ondelete(item)} />
+      <span onClick={toggleDone} className={Done ? "done" : ""}>
+        {text}
+      </span>
+      <MdDelete className="delete" onClick={() => ondelete(_id)} />
     </div>
   );
 };
